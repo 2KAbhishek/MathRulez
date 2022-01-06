9d49f1f33e2403d08bda21cd4e3f0a5fdbd488db
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-06 22:00:42 +0530

    Complete results activity

--- app/src/main/java/com/iam2kabhishek/mathrulez/ResultsActivity.kt
+++ app/src/main/java/com/iam2kabhishek/mathrulez/ResultsActivity.kt
@@ -1,11 +1,25 @@
 package com.iam2kabhishek.mathrulez
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_results.*
 
 class ResultsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_results)
+
+        val userName = intent.getStringExtra(Constants.USER_NAME)
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 14)
+
+        tv_user_name.text = userName
+        "You scored $correctAnswers out of $totalQuestions".also { tv_score.text = it }
+
+        btn_again.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
