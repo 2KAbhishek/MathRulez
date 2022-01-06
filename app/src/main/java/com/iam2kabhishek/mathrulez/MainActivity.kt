28d41e89811cc18071a370bddb79540afffe1f2c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-06 17:36:13 +0530

    Only allow moving forward if name is entered

--- app/src/main/java/com/iam2kabhishek/mathrulez/MainActivity.kt
+++ app/src/main/java/com/iam2kabhishek/mathrulez/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.iam2kabhishek.mathrulez
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener{
+            if (et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
     }
 }
