76041283fdfa0c50c0c6e4c9498a79a9a3d5e451
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-01-06 21:19:08 +0530

    Add results activity

---
+++ app/src/main/java/com/iam2kabhishek/mathrulez/Results.kt
@@ -0,0 +1,11 @@
+package com.iam2kabhishek.mathrulez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Results : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_results)
+    }
+}
